08e0ef7bdab9759c9f3715a9b9129eaa5d7ed3a0
sukai <sukai@gamegoing.net>
2022-08-27 14:06:25 +0800

    修改内网IP

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -39,7 +39,7 @@ class NetworkConfig {
       case ChannelConfig.channelInternetProxy:
         return "8.218.97.215:8080";
       case ChannelConfig.channelIntranet:
-        return "192.168.1.7:8080";
+        return "192.168.1.2:8080";
     }
     // return "172.17.22.65:8080";
     return "127.0.0.1:8080";
