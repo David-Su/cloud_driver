be4bdb57b34f9541027512e7d4978ba06a016b1e
SuK <543879736@qq.com>
2024-10-11 16:19:55 +0800

    commit

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -43,10 +43,10 @@ class NetworkConfig {
       case ChannelConfig.channelInternetProxy:
         return "www.fqym.top:8080";
       case ChannelConfig.channelIntranet:
-        return "192.168.0.250:8080";
+        return "127.0.0.1:8080";
     }
-    return "192.168.0.107:8080";
-    // return "192.168.0.250:8080";
+    // return "127.0.0.1:8080";
+    return "192.168.20.105:8080";
   }
 
   static String get _httpScheme {
